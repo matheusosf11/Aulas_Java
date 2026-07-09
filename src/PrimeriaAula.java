405b6add68701784c3b4b1aaf9af8829add86c27
matheus <matheusosf11@gmail.com>
2026-07-09 20:43:55 -0300

    Adicionando arquivos no repositório

---
+++ src/PrimeriaAula.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class PrimeriaAula {
+    public static void main(String[] args){
+        String nome;
+        int idade;
+        double altura;
+        String cargo;
+        String periodo;
+        
+        Scanner entrada1 = new Scanner(System.in);
+        Scanner entrada2  = new Scanner(System.in);
+        Scanner entrada3 = new Scanner(System.in);
+        Scanner entrada4 = new Scanner(System.in);
+        Scanner entrada5 = new Scanner(System.in);
+
+        System.out.println("Digite seu nome completo: ");
+        nome = entrada1.nextLine();
+        System.out.println("Digite sua idade: ");
+         idade = entrada2.nextInt();
+        System.out.println("Digite sua altura: ");
+         altura = entrada3.nextDouble();
+        System.out.println("Digite seu cargo:  ");
+         cargo = entrada4.nextLine();
+        System.out.println("Digite seu periodo do curso:  ");
+         periodo = entrada5.nextLine();
+
+
+
+
+    }
+
+
+
+
+}
